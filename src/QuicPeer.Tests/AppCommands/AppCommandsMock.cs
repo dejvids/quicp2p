8181ad46bd025aa0fdaa89f746f6db3b98d64198
dawid <dawidsurys@hotmail.com>
2026-01-12 20:19:20 +0100

    Unit tests workflow completion

--- src/QuicPeer.Tests/AppCommands/AppCommandsMock.cs
+++ src/QuicPeer.Tests/AppCommands/AppCommandsMock.cs
@@ -1,4 +1,6 @@
-﻿using QuicPeer.AppCommands;
+﻿using Microsoft.Extensions.Logging;
+using NSubstitute;
+using QuicPeer.AppCommands;
 
 namespace QuicPeer.Tests.AppCommands;
 
@@ -7,11 +9,10 @@ public static class AppCommandsMock
     public static ConnectCommand ConnectCommand { get; } = new ConnectCommandMock();
     public static ShowDataCommand ShowDataCommand { get; } = new ShowDataCommandMock();
 
-
     public class ConnectCommandMock : ConnectCommand
     {
         public ConnectCommandMock()
-            : base(null, null, null, null, null)
+            : base(Substitute.For<ILogger<ConnectCommand>>(), Substitute.For<IConsoleAccessor>(), null, new SendCommandMock(), new SendFileCommandMock())
         {
         }
 
@@ -24,13 +25,29 @@ public static class AppCommandsMock
     public class ShowDataCommandMock : ShowDataCommand
     {
         public ShowDataCommandMock()
-            : base(null, null)
+            : base(Substitute.For<ILogger<ShowDataCommand>>(), Substitute.For<IConsoleAccessor>())
         {
         }
 
         public override ValueTask Execute(CancellationToken cancellationToken)
         {
             return ValueTask.CompletedTask;
+        }
+    }
+
+    public class SendCommandMock : SendCommand
+    {
+        public SendCommandMock() 
+            : base(Substitute.For<ILogger<SendCommand>>(), Substitute.For<IConsoleAccessor>())
+        {
+        }
+    }
+
+    public class SendFileCommandMock : SendFileCommand
+    {
+        public SendFileCommandMock()
+            : base(Substitute.For<ILogger<SendFileCommand>>(), Substitute.For<IConsoleAccessor>())
+        {
         }
     }
 
